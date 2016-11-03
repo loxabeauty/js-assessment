b3e16622a59dec2c1620335c98fe33111808796e
KingHippo <askdonno@gmail.com>
2016-11-03 18:49:15 -0400

    solution for bestPractices.js

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -9,18 +9,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.bestPracticesAnswers = {
   globals: function() {
-    myObject = {
+    let myObject = {
       name: 'Jory'
     };
-
     return myObject;
   },
 
   parseInt: function(num) {
-    return parseInt(num);
+    console.log(num);
+    return parseInt(num, 10);
   },
 
   identity: function(val1, val2) {
 
+  return val1 === val2;
   }
 };
