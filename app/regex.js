ce5a997da192f4af98fe6c2fc59dbbc6cdbf4aa0
KingHippo <askdonno@gmail.com>
2016-11-03 20:49:48 -0400

    solution for containsNumber and captureThreeNumbers

--- app/regex.js
+++ app/regex.js
@@ -3,11 +3,7 @@ exports = typeof window === 'undefined' ? global : window;
 exports.regexAnswers = {
   containsNumber: function(str) {
     var regex = /\d/;
-    if (str.match(regex)){
-      return true;
-    } else {
-      return false;
-    }
+    return str.match(regex) ? true : false;
   },
 
   containsRepeatingLetter: function(str) {
@@ -20,8 +16,9 @@ exports.regexAnswers = {
 
   captureThreeNumbers: function(str) {
     var regex = /\d{3}/;
+    console.log(str.match(regex));
   //  console.log(str.match(regex));
-    return str.match(regex);
+    return str.match(regex) ? str.match(regex).toString() : false;
   },
 
   matchesPattern: function(str) {
