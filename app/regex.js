d6c1226af6e9192d6c780f236fe1227a88775eed
KingHippo <askdonno@gmail.com>
2016-11-03 20:30:15 -0400

    solution for containsNumber

--- app/regex.js
+++ app/regex.js
@@ -2,7 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    var regex = /\d/;
+    if (str.match(regex)){
+      return true;
+    } else {
+      return false;
+    }
   },
 
   containsRepeatingLetter: function(str) {
@@ -14,7 +19,9 @@ exports.regexAnswers = {
   },
 
   captureThreeNumbers: function(str) {
-
+    var regex = /\d{3}/;
+  //  console.log(str.match(regex));
+    return str.match(regex);
   },
 
   matchesPattern: function(str) {
