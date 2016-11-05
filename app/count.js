d5948a64fb92afaee6a7b9e86ce1b8a8fcfbbbfb
KingHippo <askdonno@gmail.com>
2016-11-05 03:08:54 -0400

    attempt to solve count

--- app/count.js
+++ app/count.js
@@ -2,6 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    var count = start;
+    function finalCountDown(){
+      var timerId = setTimeout(function() {
+        if (count <= end) {
+          count++;
+        }
+      }, 100);
+    }
+    console.log(count);
+    return count;
+    finalCountDown(count);
   }
 };
