354421f39879253fd6239f955c54891597a1c3f3
KingHippo <askdonno@gmail.com>
2016-11-05 03:05:27 -0400

    create reportStatus function

--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    // console.log(value);
   },
 
   manipulateRemoteData: function(url) {
-
+    function reportStatus()
+{
+  if (oReq.readyState == 4 /* complete */) {
+      if (oReq.status == 200 || oReq.status == 304) {
+            alert('Transfer complete.');
+        }
+      else {
+            // error occurred
+        }
+    }
+    }
+    var oReq = new XMLHttpRequest();
+    oReq.open("GET", "http://localhost/test.xml", true);
+    oReq.onreadystatechange = reportStatus;
+    oReq.send();
   }
 };
