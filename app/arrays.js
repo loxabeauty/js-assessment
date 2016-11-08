48bc04fab18536b733d17f5b41842f7748a2c61d
rrana <rrana@iupui.edu>
2016-11-08 11:50:56 -0500

    Solution Array.js

--- app/arrays.js
+++ app/arrays.js
@@ -2,6 +2,10 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
+	
+	//indexOf() method returns the first index at which a given element can be found in the array
+	//Array.prototype property represents the prototype for the Array constructor and allows 
+	//to add new properties and methods to all Array objects
 	if (Array.prototype.indexOf) {
 		return arr.indexOf(item);
 		} else {
@@ -24,7 +28,7 @@ exports.arraysAnswers = {
                 if(isNaN(arr[i])){
                 continue;
                 } // end if
-                 sum1 += Number(arr[i]);
+                 sum1 =sum1 + Number(arr[i]);
                } //end for
         return sum1;
   },
@@ -38,7 +42,7 @@ exports.arraysAnswers = {
   removeWithoutCopy: function(arr, item) {
 	for (var i = 0; i < arr.length; i++) {
 		if (arr[i] === item) {
-			arr.splice(i, 1);
+			arr.splice(i, 1); 	//splice() method changes the content of an array by removing existing elements or adding new elements
 			i--;
 		}
 	}
@@ -46,22 +50,22 @@ exports.arraysAnswers = {
   },
 
   append: function(arr, item) {
-	arr.push(item);
+	arr.push(item);		//pushes the item at the end of an array
  return arr;
   },
 
   truncate: function(arr) {
-	arr.pop();
+	arr.pop();			// pops the first element of the given array
  return arr;
   },
 
   prepend: function(arr, item) {
-	arr.unshift(item);
+	arr.unshift(item);	//adds one or more elements to the beginning of an array and returns the new length of the array
  return arr;
   },
 
   curtail: function(arr) {
-	arr.shift();
+	arr.shift();		// removes the first element from an array and returns that element
  return arr;
   },
 
@@ -94,7 +98,7 @@ exports.arraysAnswers = {
   },
 
   square: function(arr) {
-	ans= [];
+	ans= [];	// declare an array to store the square values of the given array
 
     for (var i = 0, len = arr.length; i < len; i++) {
         ans.push(arr[i] * arr[i]);
@@ -104,8 +108,8 @@ exports.arraysAnswers = {
   },
 
   findAllOccurrences: function(arr, target) {
-	 var indexes = [];
-  var i = -1;
+	  var indexes = [];
+	  var i = -1;
       
 	while ((i = arr.indexOf(target, i+1)) != -1){
         indexes.push(i);
