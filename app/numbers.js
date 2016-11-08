e718f2e1a9a935114d72671ce215046576e3af63
rrana <rrana@iupui.edu>
2016-11-08 14:51:09 -0500

    Solution numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -12,17 +12,22 @@ exports.numbersAnswers = {
   convertToBinary: function(num) {
 	function byteString(n) {
 		if (n < 0 || n > 255 || n % 1 !== 0) {
-			 throw new Error(n + " does not fit in a byte");}
-		return ("000000000" + n.toString(2)).substr(-8)}
-		return byteString(num);
+			 throw new Error(n + " does not fit in a byte");
+			 }
+		return ("000000000" + n.toString(2)).substr(-8);
+		}
+		return byteString(num); 
+		 //return (num >>> 0).toString(2);
+		
   },
 
   multiply: function(a, b) {
 	var multiply = function (a, b) {
-		 var commonMultiplier = 1000000;
-		 a *= commonMultiplier;
-		  b *= commonMultiplier;
-		  return (a * b) / (commonMultiplier * commonMultiplier);
+		 var x = 1000000;
+		 var div = x*x;
+		 a = a*x;
+		 b = b*x;
+		  return (a * b) / div;
 		  };
 		  return multiply(a, b)
   }
