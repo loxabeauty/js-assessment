74a0285eebddd4fae940739bfca84160bc3f7a32
rrana <rrana@iupui.edu>
2016-11-08 12:28:14 -0500

    Solution bestPractics.js

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -9,7 +9,7 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.bestPracticesAnswers = {
   globals: function() {
-   var myObject = {
+   var myObject = {		// myObject should be assigned to a variable
       name: 'Jory'
     };
 
@@ -20,18 +20,18 @@ exports.bestPracticesAnswers = {
   functions : function(flag) {
      var getValue;
      if (flag) {
-       getValue = function () { return 'a'; }
+       getValue = function () { return 'x'; } 		// assign o/p of a funtion to a variable
      } else {
-       getValue = function () { return 'b'; }
+       getValue = function () { return 'y'; }
      }
  
      return getValue();
    },
  
-  
-  
+    
   parseInt: function(num) {
-    return parseInt(num, 10);
+    return parseInt(num, 10);		//parseInt() function parses a string argument and returns an integer of the specified radix 
+									//Syntax: parseInt(string, radix); where 2<= radix<=36
   },
 
   identity: function(val1, val2) {
